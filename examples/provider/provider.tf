be12b653006d9c9973aed3f304a4d843eb86bcdf
John Esterline <john.esterline@illumio.com>
2024-06-21 11:52:18 -0700

    Improve the docs
    Update docs/index.md.
    Updating examples/provider/provider.tf.

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,14 @@
-provider "scaffolding" {
-  # example configuration here
+# Configure the Illumio CloudSecure provider using the required_providers stanza.
+terraform {
+  required_providers {
+    illumio-cloudsecure = {
+      source  = "illumio/illumio-cloudsecure"
+      version = "~> 0.1"
+    }
+  }
+}
+
+# Onboard an AWS account
+resource "illumio-cloudsecure_aws_account" "example" {
+  # ...
 }
