7e6697c72bf1327de4863d56f28c272c3233fff0
Shireesh Gadidesi <shireesh.gadidesi@illumio.com>
2024-06-26 20:35:29 -0700

    Pull request #11: adding http authentication CLOUD-3524
    Merge in CLOUD/terraform-provider-illumio-cloudsecure from auth_fakeserver to main
    
    Squashed commit of the following:
    
    commit e8cc9fd53def9708a1aad1a8232c8f4765a56671
    Author: shireesh <reachme@shireesh.com>
    Date:   Wed Jun 26 20:35:11 2024 -0700
    
        comments fix
    
    commit 8812aaced687abc32207b9a05250bd95fb2536e4
    Author: shireesh <reachme@shireesh.com>
    Date:   Wed Jun 26 13:55:34 2024 -0700
    
        naming
    
    commit facfccada48a26db1e0430513ceb9490aaa3fe63
    Author: shireesh <reachme@shireesh.com>
    Date:   Wed Jun 26 13:01:16 2024 -0700
    
        review comments
    
    commit e7ea47e0a2755e92471098e97d91798119af5127
    Author: shireesh <reachme@shireesh.com>
    Date:   Wed Jun 26 12:57:25 2024 -0700
    
        review comments
    
    commit 2628f2e210d3fe098317d6f6c7db493722b97944
    Author: shireesh <reachme@shireesh.com>
    Date:   Tue Jun 25 20:19:34 2024 -0700
    
        adding http authentication
    
    commit 558bb94ea1787afb39244d00911a61e8571820f3
    Author: shireesh <reachme@shireesh.com>
    Date:   Tue Jun 25 20:07:49 2024 -0700
    
         adding http authentication

---
+++ GNUmakefile
@@ -0,0 +1,56 @@
+# Copyright (c) Illumio, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+BINARY_NAME=terraform-provider-illumio-cloudsecure
+VERSION=0.0.1
+PLUGIN_DIR=~/.terraform.d/plugins/registry.terraform.io/illumio/illumio-cloudsecure
+OS ?= $(shell uname -s | tr '[:upper:]' '[:lower:]')
+ARCH ?= $(shell uname -m)
+ifeq ($(ARCH),x86_64)
+    ARCH=amd64
+else ifeq ($(ARCH),arm64)
+    ARCH=arm64
+else ifeq ($(ARCH),aarch64)
+    ARCH=arm64
+endif
+TF_PLUGIN_DIR = $(OS)_$(ARCH)
+
+default: testacc
+
+# Run acceptance tests
+.PHONY: testacc clean-build clean generate build run reset lint
+
+testacc:
+	TF_ACC=1 go test ./... -v $(TESTARGS) -timeout 120m
+
+
+clean: clean-build
+	find . -name "*.gen.go" -exec rm -f {} \;
+	find . -name "*.pb.go" -exec rm -f {} \;
+	find . -name "*config.proto" -exec rm -f {} \;
+	rm -rf ./docs
+	rm -rf ./terraform-provider-illumio-cloudsecure
+
+clean-build:
+	rm -rf ~/.terraform.d/plugins/registry.terraform.io/illumio/illumio-cloudsecure/
+
+generate:
+	(cd api ; go generate) && \
+    (cd internal/provider ; go generate) && \
+    (cd fakeserver ; go generate) && \
+    (go generate ./...)
+
+build:
+	# Create the appropriate directory and copy the built binary
+	mkdir -p $(PLUGIN_DIR)/$(VERSION)/$(TF_PLUGIN_DIR)
+	# Build the Go project
+	go build -o $(PLUGIN_DIR)/$(VERSION)/$(TF_PLUGIN_DIR)/$(BINARY_NAME)
+
+
+run:
+	go run ./fakeserver/ -apiEndpoint 0.0.0.0:50123 -tokenEndpoint 0.0.0.0:50124
+
+reset: clean generate build
+
+lint: 
+	golangci-lint run
--- fakeserver/main.go
+++ fakeserver/main.go
@@ -4,15 +4,50 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"net"
+	"strings"
 
 	configv1 "github.com/illumio/terraform-provider-illumio-cloudsecure/api/illumio/cloud/config/v1"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
+const (
+	AuthorizationHeader = "authorization"
+	DefaultToken        = "token1"
+	DefaultClientID     = "client_id_1"
+	DefaultClientSecret = "client_secret_1"
+)
+
+func tokenAuthInterceptor(token string) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		_ *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (any, error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, status.Errorf(codes.Unauthenticated, "metadata is not provided")
+		}
+
+		authHeader, ok := md[AuthorizationHeader]
+
+		authHeader = strings.Split(authHeader[0], " ")
+		if !ok || authHeader[0] != "Bearer" || len(authHeader) != 2 || authHeader[1] != token {
+			return nil, status.Errorf(codes.Unauthenticated, "invalid token")
+		}
+
+		return handler(ctx, req)
+	}
+}
 func main() {
 	var (
 		// debug enables debug logging if true.
@@ -21,13 +56,21 @@ func main() {
 		// network is the network used to access the gRPC server.
 		network string
 
-		// address is the gRPC server address.
-		address string
+		// token is the token used for auth by fake server.
+		token string
+
+		// apiEndpoint is the address of the Config API endpoint.
+		apiEndpoint string
+
+		// tokenEndpoint is the address of the OAuth 2 Token endpoint.
+		tokenEndpoint string
 	)
 
 	flag.BoolVar(&debug, "debug", false, "enables debug logging")
 	flag.StringVar(&network, "network", "tcp", "network of the address of the gRPC server, e.g., \"tcp\" or \"unix\"")
-	flag.StringVar(&address, "address", "127.0.0.1:50123", "address of the gRPC server")
+	flag.StringVar(&token, "token", "", "token used for auth by fake server. If not provided, a default token will be used.")
+	flag.StringVar(&apiEndpoint, "apiEndpoint", "127.0.0.1:50123", "address of the Config API endpoint")
+	flag.StringVar(&tokenEndpoint, "tokenEndpoint", "127.0.0.1:50124", "address of the OAuth 2 Token endpoint")
 	flag.Parse()
 
 	var logger *zap.Logger
@@ -40,21 +83,39 @@ func main() {
 		logger, err = zap.NewProduction()
 	}
 
+	if token == "" {
+		token = DefaultToken
+	}
+
 	if err != nil {
 		panic(fmt.Sprintf("failed to configure logger: %s", err))
 	}
 
-	listener, err := net.Listen(network, address)
+	listener, err := net.Listen(network, apiEndpoint)
 	if err != nil {
 		logger.Fatal("failed to open network port", zap.Error(err))
 	}
 
 	// nosemgrep: go.grpc.security.grpc-server-insecure-connection.grpc-server-insecure-connection
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(tokenAuthInterceptor(token)),
+	)
 	configv1.RegisterConfigServiceServer(server, NewFakeConfigServer(logger))
-	logger.Info("server listening", zap.String("network", listener.Addr().Network()), zap.String("address", listener.Addr().String()))
+	logger.Info("api server listening", zap.String("network", listener.Addr().Network()), zap.String("address", listener.Addr().String()))
+	reflection.Register(server)
 
-	if err = server.Serve(listener); err != nil {
+	go func() {
+		startHTTPServer(
+			tokenEndpoint,
+			logger,
+			DefaultClientID,
+			DefaultClientSecret,
+			token,
+		)
+	}()
+	logger.Info("token endpoint listening", zap.String("address", tokenEndpoint))
+
+	if err := server.Serve(listener); err != nil {
 		logger.Fatal("server failed", zap.Error(err))
 	}
 }
