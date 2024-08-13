95e2fddb231a04fdcf86949ef003261cf31cad6b
Romain Lenglet <romain.lenglet@berabera.info>
2024-08-13 23:18:45 +0000

    Update resource examples

--- examples/resources/illumio-cloudsecure_aws_account/resource.tf
+++ examples/resources/illumio-cloudsecure_aws_account/resource.tf
@@ -1,24 +1,10 @@
 resource "illumio-cloudsecure_aws_account" "account_example" {
   account_id       = "812713887999"
-  account_type     = "Account"
   name             = "Test AWS Account"
   role_arn         = "arn:aws:iam::812713887999:role/IllumioAccess"
   role_external_id = "eb287482f5824fab8a6988252d56eb6d"
 
   # Optional attributes
-  mode = "ReadWrite"
-}
-
-
-resource "illumio-cloudsecure_aws_account" "org_example" {
-  account_id            = "812713887999"
-  account_type          = "Organization"
-  name                  = "Test AWS Org"
-  role_arn              = "arn:aws:iam::812713887999:role/IllumioAccess"
-  role_external_id      = "eb287482f5824fab8a6988252d56eb6d"
-  management_account_id = "965208753613"
-  organization_id       = "o-3eehyj6qk0"
-
-  # Optional attributes
-  mode = "ReadWrite"
+  organization_account_id = "965208753613"
+  mode                    = "ReadWrite"
 }
---
+++ examples/resources/illumio-cloudsecure_aws_organization/resource.tf
@@ -0,0 +1,10 @@
+resource "illumio-cloudsecure_aws_organization" "organization_example" {
+  account_id            = "965208753613"
+  organization_id       = "o-3eehyj6qk0"
+  name                  = "Test AWS Organization"
+  role_arn              = "arn:aws:iam::965208753613:role/IllumioAccess"
+  role_external_id      = "eb287482f5824fab8a6988252d56eb6d"
+
+  # Optional attributes
+  mode = "ReadWrite"
+}
