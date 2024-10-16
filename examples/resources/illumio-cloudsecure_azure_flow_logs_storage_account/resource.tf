a27680472d3381ae0618784155d66b3dcd36eb4d
Busi Reddy <busisatya@gmail.com>
2024-10-16 22:13:09 +0000

    Added example for azure flowlogs storage account, updated docs

---
+++ examples/resources/illumio-cloudsecure_azure_flow_logs_storage_account/resource.tf
@@ -0,0 +1,4 @@
+resource "illumio-cloudsecure_azure_flow_logs_storage_account" "storage_account_example" {
+  subscription_id             = "c219f111-9005-45d4-8bb3-4d50120d3ef2"
+  storage_account_resource_id = "/subscriptions/randomids-d469-aghg-a4b4-asdsdasadas/resourceGroups/azrg-illumio/providers/Microsoft.Storage/storageAccounts/illumioazuretest"
+}
