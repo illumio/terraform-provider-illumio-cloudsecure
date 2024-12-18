36976a2db64d10540df656193589a5e664e21465
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2024-12-18 11:17:19 -0800

    test cases

--- api/providergen/providergen_test.go
+++ api/providergen/providergen_test.go
@@ -262,4 +262,205 @@ func (suite *GenerateProviderSuite) TestSetsAndMore() {
 	suite.Require().NoError(err, "ProviderConvertersTemplate.Execute should not return an error")
 
 	suite.NotEmpty(dst.String(), "Generated provider output should not be empty")
+
+	expectedVal := `
+type NestedObjectTester_CloudTags struct {
+	Cloud types.String ` + "`" + `tfsdk:"cloud"` + "`" + `
+	Key types.String ` + "`" + `tfsdk:"key"` + "`" + `
+}
+func GetTypeAttrsForNestedObjectTester_CloudTags() map[string]attr.Type {
+	return map[string]attr.Type{
+		"cloud": types.StringType,
+		"key": types.StringType,
+	}
+}
+
+
+func ConvertNestedObjectTester_CloudTagsToObjectValueFromProto(proto *configv1.NestedObjectTester_CloudTags) basetypes.ObjectValue  {
+	return types.ObjectValueMust(
+		GetTypeAttrsForNestedObjectTester_CloudTags(),
+		map[string]attr.Value{
+			"cloud": types.StringValue(proto.Cloud),
+			"key": types.StringValue(proto.Key),
+		},
+	)
+}
+func ConvertDataValueToNestedObjectTester_CloudTagsProto(ctx context.Context, dataValue attr.Value) *configv1.NestedObjectTester_CloudTags {
+	pv := NestedObjectTester_CloudTags{}
+	diags := tfsdk.ValueAs(context.Background(), dataValue, &pv)
+	if len(diags) > 0 {
+		tflog.Error(ctx, "Unexpected diagnostics", map[string]any{"diags": diags})
+	}
+	proto := &configv1.NestedObjectTester_CloudTags{}
+	proto.Cloud = pv.Cloud.ValueString()
+	proto.Key = pv.Key.ValueString()
+	return proto
+}
+type NestedObjectTester_Icon struct {
+	BackgroundColor types.String ` + "`" + `tfsdk:"background_color"` + "`" + `
+	ForegroundColor types.String ` + "`" + `tfsdk:"foreground_color"` + "`" + `
+	Name types.String ` + "`" + `tfsdk:"name"` + "`" + `
+}
+func GetTypeAttrsForNestedObjectTester_Icon() map[string]attr.Type {
+	return map[string]attr.Type{
+		"background_color": types.StringType,
+		"foreground_color": types.StringType,
+		"name": types.StringType,
+	}
+}
+
+
+func ConvertNestedObjectTester_IconToObjectValueFromProto(proto *configv1.NestedObjectTester_Icon) basetypes.ObjectValue  {
+	return types.ObjectValueMust(
+		GetTypeAttrsForNestedObjectTester_Icon(),
+		map[string]attr.Value{
+			"background_color": types.StringValue(proto.BackgroundColor),
+			"foreground_color": types.StringValue(proto.ForegroundColor),
+			"name": types.StringValue(proto.Name),
+		},
+	)
+}
+func ConvertDataValueToNestedObjectTester_IconProto(ctx context.Context, dataValue attr.Value) *configv1.NestedObjectTester_Icon {
+	pv := NestedObjectTester_Icon{}
+	diags := tfsdk.ValueAs(context.Background(), dataValue, &pv)
+	if len(diags) > 0 {
+		tflog.Error(ctx, "Unexpected diagnostics", map[string]any{"diags": diags})
+	}
+	proto := &configv1.NestedObjectTester_Icon{}
+	proto.BackgroundColor = pv.BackgroundColor.ValueString()
+	proto.ForegroundColor = pv.ForegroundColor.ValueString()
+	proto.Name = pv.Name.ValueString()
+	return proto
+}
+type NestedObjectTester_ObjInObj struct {
+	Child types.Object ` + "`" + `tfsdk:"child"` + "`" + `
+	Name types.String ` + "`" + `tfsdk:"name"` + "`" + `
+}
+func GetTypeAttrsForNestedObjectTester_ObjInObj() map[string]attr.Type {
+	return map[string]attr.Type{
+		"child": types.ObjectType{
+			AttrTypes: GetTypeAttrsForNestedObjectTester_ObjInObj_Child(),
+		},
+		"name": types.StringType,
+	}
+}
+
+
+func ConvertNestedObjectTester_ObjInObjToObjectValueFromProto(proto *configv1.NestedObjectTester_ObjInObj) basetypes.ObjectValue  {
+	return types.ObjectValueMust(
+		GetTypeAttrsForNestedObjectTester_ObjInObj(),
+		map[string]attr.Value{
+			"child": ConvertNestedObjectTester_ObjInObj_ChildToObjectValueFromProto(proto.Child),
+			"name": types.StringValue(proto.Name),
+		},
+	)
+}
+func ConvertDataValueToNestedObjectTester_ObjInObjProto(ctx context.Context, dataValue attr.Value) *configv1.NestedObjectTester_ObjInObj {
+	pv := NestedObjectTester_ObjInObj{}
+	diags := tfsdk.ValueAs(context.Background(), dataValue, &pv)
+	if len(diags) > 0 {
+		tflog.Error(ctx, "Unexpected diagnostics", map[string]any{"diags": diags})
+	}
+	proto := &configv1.NestedObjectTester_ObjInObj{}
+	proto.Child = ConvertDataValueToNestedObjectTester_ObjInObj_ChildProto(ctx, pv.Child)
+	proto.Name = pv.Name.ValueString()
+	return proto
+}
+type NestedObjectTester_ObjInObj_Child struct {
+	GrandChild types.Object ` + "`" + `tfsdk:"grand_child"` + "`" + `
+	Name types.String ` + "`" + `tfsdk:"name"` + "`" + `
+}
+func GetTypeAttrsForNestedObjectTester_ObjInObj_Child() map[string]attr.Type {
+	return map[string]attr.Type{
+		"grand_child": types.ObjectType{
+			AttrTypes: GetTypeAttrsForNestedObjectTester_ObjInObj_Child_GrandChild(),
+		},
+		"name": types.StringType,
+	}
+}
+
+
+func ConvertNestedObjectTester_ObjInObj_ChildToObjectValueFromProto(proto *configv1.NestedObjectTester_ObjInObj_Child) basetypes.ObjectValue  {
+	return types.ObjectValueMust(
+		GetTypeAttrsForNestedObjectTester_ObjInObj_Child(),
+		map[string]attr.Value{
+			"grand_child": ConvertNestedObjectTester_ObjInObj_Child_GrandChildToObjectValueFromProto(proto.GrandChild),
+			"name": types.StringValue(proto.Name),
+		},
+	)
+}
+func ConvertDataValueToNestedObjectTester_ObjInObj_ChildProto(ctx context.Context, dataValue attr.Value) *configv1.NestedObjectTester_ObjInObj_Child {
+	pv := NestedObjectTester_ObjInObj_Child{}
+	diags := tfsdk.ValueAs(context.Background(), dataValue, &pv)
+	if len(diags) > 0 {
+		tflog.Error(ctx, "Unexpected diagnostics", map[string]any{"diags": diags})
+	}
+	proto := &configv1.NestedObjectTester_ObjInObj_Child{}
+	proto.GrandChild = ConvertDataValueToNestedObjectTester_ObjInObj_Child_GrandChildProto(ctx, pv.GrandChild)
+	proto.Name = pv.Name.ValueString()
+	return proto
+}
+type NestedObjectTester_ObjInObj_Child_GrandChild struct {
+	Name types.String ` + "`" + `tfsdk:"name"` + "`" + `
+}
+func GetTypeAttrsForNestedObjectTester_ObjInObj_Child_GrandChild() map[string]attr.Type {
+	return map[string]attr.Type{
+		"name": types.StringType,
+	}
+}
+
+
+func ConvertNestedObjectTester_ObjInObj_Child_GrandChildToObjectValueFromProto(proto *configv1.NestedObjectTester_ObjInObj_Child_GrandChild) basetypes.ObjectValue  {
+	return types.ObjectValueMust(
+		GetTypeAttrsForNestedObjectTester_ObjInObj_Child_GrandChild(),
+		map[string]attr.Value{
+			"name": types.StringValue(proto.Name),
+		},
+	)
+}
+func ConvertDataValueToNestedObjectTester_ObjInObj_Child_GrandChildProto(ctx context.Context, dataValue attr.Value) *configv1.NestedObjectTester_ObjInObj_Child_GrandChild {
+	pv := NestedObjectTester_ObjInObj_Child_GrandChild{}
+	diags := tfsdk.ValueAs(context.Background(), dataValue, &pv)
+	if len(diags) > 0 {
+		tflog.Error(ctx, "Unexpected diagnostics", map[string]any{"diags": diags})
+	}
+	proto := &configv1.NestedObjectTester_ObjInObj_Child_GrandChild{}
+	proto.Name = pv.Name.ValueString()
+	return proto
+}
+type NestedObjectTester_SetObj struct {
+	SetKey types.String ` + "`" + `tfsdk:"set_key"` + "`" + `
+	SetVal types.String ` + "`" + `tfsdk:"set_val"` + "`" + `
+}
+func GetTypeAttrsForNestedObjectTester_SetObj() map[string]attr.Type {
+	return map[string]attr.Type{
+		"set_key": types.StringType,
+		"set_val": types.StringType,
+	}
+}
+
+
+func ConvertNestedObjectTester_SetObjToObjectValueFromProto(proto *configv1.NestedObjectTester_SetObj) basetypes.ObjectValue  {
+	return types.ObjectValueMust(
+		GetTypeAttrsForNestedObjectTester_SetObj(),
+		map[string]attr.Value{
+			"set_key": types.StringValue(proto.SetKey),
+			"set_val": types.StringValue(proto.SetVal),
+		},
+	)
+}
+func ConvertDataValueToNestedObjectTester_SetObjProto(ctx context.Context, dataValue attr.Value) *configv1.NestedObjectTester_SetObj {
+	pv := NestedObjectTester_SetObj{}
+	diags := tfsdk.ValueAs(context.Background(), dataValue, &pv)
+	if len(diags) > 0 {
+		tflog.Error(ctx, "Unexpected diagnostics", map[string]any{"diags": diags})
+	}
+	proto := &configv1.NestedObjectTester_SetObj{}
+	proto.SetKey = pv.SetKey.ValueString()
+	proto.SetVal = pv.SetVal.ValueString()
+	return proto
+}
+`
+	suite.Equal(expectedVal, dst.String(), "Generated provider output should match the expected output")
+
 }
