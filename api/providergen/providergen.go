5086ede9ff630d1a4bbe268ee776075f8013c502
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2025-01-08 15:31:27 -0800

    fix ctx and diags

--- api/providergen/providergen.go
+++ api/providergen/providergen.go
@@ -269,7 +269,7 @@ type {{.Name}} struct {
 {{- end}}
 {{- define "convertDataValueToProto"}}
 	{{- if ne .NestedModel nil}}
-		protoValue, newDiags := ConvertDataValueTo{{.NestedModel.Name}}Proto(dataValue)
+		protoValue, newDiags := ConvertDataValueTo{{.NestedModel.Name}}Proto(ctx, dataValue)
 		diags.Append(newDiags...)
 	{{- else if eq .CollectionElementType nil}}
 		protoValue = dataValue.(types.{{.ModelTypeName}}).Value{{.ModelTypeName}}()
