9c0cd20e4f328fdfc9b0ebdd923f3408b66b19cf
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2025-01-27 10:28:47 -0800

    immutable deploymentId

--- api/schema/application_resource.go
+++ api/schema/application_resource.go
@@ -15,17 +15,22 @@ var (
 			MarkdownDescription: "An application in the Illumio CloudSecure platform.",
 			Attributes: map[string]resource_schema.Attribute{
 				IDFieldName: idAttribute,
-				"name": resource_schema.StringAttribute{
-					MarkdownDescription: "Display name of the created CloudSecure application.",
-					Required:            true,
-				},
-				"deployment_id": resource_schema.StringAttribute{
-					MarkdownDescription: "ID of the Cloudsecure deployment.",
-					Required:            true,
+				"deployment_id": StringResourceAttributeWithMode{
+					StringAttribute: resource_schema.StringAttribute{
+						MarkdownDescription: "ID of the Cloudsecure deployment.",
+						Required:            true,
+					},
+					attributeWithMode: attributeWithMode{
+						Mode: ImmutableAttributeMode,
+					},
 				},
 				"description": resource_schema.StringAttribute{
 					MarkdownDescription: "Description of the created application.",
 					Optional:            true,
+				},
+				"name": resource_schema.StringAttribute{
+					MarkdownDescription: "Display name of the created CloudSecure application.",
+					Required:            true,
 				},
 			},
 		},
