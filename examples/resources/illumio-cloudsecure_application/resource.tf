3fd118b812acedd5cecc8f60dd7270967b21e443
shireesh <shireesh.gadidesi+git@illumio.com>
2025-03-02 20:28:37 -0800

    Add examples for application definition resources (#160)
    Add examples for the `application`, `deployment`,
    `application_aws_resources`, and `application_azure_resources`
    resources.
    
    ---------
    
    Co-authored-by: shireesh gadidesi <shireesh.gadidesi@illumio.com>
    Co-authored-by: Romain Lenglet <romain.lenglet@berabera.info>

---
+++ examples/resources/illumio-cloudsecure_application/resource.tf
@@ -0,0 +1,18 @@
+data "azurerm_subscription" "current" {}
+data "aws_caller_identity" "current" {}
+
+# Define a deployment and an application
+
+resource "illumio-cloudsecure_deployment" "test_deployment" {
+  name                   = "Production"
+  description            = "Production deployment"
+  azure_subscription_ids = [data.azurerm_subscription.current.id]
+  aws_account_ids        = [data.aws_caller_identity.current.account_id]
+}
+
+
+resource "illumio-cloudsecure_application" "test_application" {
+  name          = "MyApplication"
+  description   = "My example application"
+  deployment_id = illumio-cloudsecure_deployment.test_deployment.id
+}
