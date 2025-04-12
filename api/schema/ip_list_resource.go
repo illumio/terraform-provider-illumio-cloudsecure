9d6b6d263e0aee32f7116c6a6085439672966847
Romain Lenglet <romain.lenglet@berabera.info>
2025-04-12 02:14:10 +0000

    Re-define the ip_list and application_policy_rule resources
    This reverts commit a76f433be37ec560fecfeba369e4fde7dd47eb7e #182.

---
+++ api/schema/ip_list_resource.go
@@ -0,0 +1,56 @@
+// Copyright (c) Illumio, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	resource_schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var IPRange = types.ObjectType{
+	AttrTypes: map[string]attr.Type{
+		"exclusion":       types.BoolType,
+		"from_ip_address": types.StringType,
+		"to_ip_address":   types.StringType,
+	},
+}
+
+var IPAddress = types.ObjectType{
+	AttrTypes: map[string]attr.Type{
+		"exclusion":  types.BoolType,
+		"ip_address": types.StringType,
+	},
+}
+
+var (
+	ipListResource = Resource{
+		TypeName: "ip_list",
+		Schema: resource_schema.Schema{
+			Version:     1,
+			Description: "Manages a list of IP address ranges to define CloudSecure policies.",
+			Attributes: map[string]resource_schema.Attribute{
+				IDFieldName: idAttribute,
+				"description": resource_schema.StringAttribute{
+					Description: "Description of the IP list.",
+					Optional:    true,
+				},
+				"ip_addresses": resource_schema.ListAttribute{
+					Optional:    true,
+					Description: "List of IP addresses.",
+					ElementType: IPAddress,
+				},
+				"ip_ranges": resource_schema.ListAttribute{
+					Optional:    true,
+					Description: "List of IP address ranges.",
+					ElementType: IPRange,
+				},
+				"name": resource_schema.StringAttribute{
+					Description: "Name of the IP list.",
+					Required:    true,
+				},
+			},
+		},
+	}
+)
