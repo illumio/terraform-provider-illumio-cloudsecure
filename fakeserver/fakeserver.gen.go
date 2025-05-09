5019e122785d754162c98ae15625f824c1e910c6
Romain Lenglet <romain.lenglet@berabera.info>
2025-05-08 17:00:14 -0700

    k8s_cluster: Temporarily remove the log_level attribute (#196)

--- fakeserver/fakeserver.gen.go
+++ fakeserver/fakeserver.gen.go
@@ -197,7 +197,6 @@ type K8SCluster struct {
 	ClientId      string
 	ClientSecret  string
 	IllumioRegion string
-	LogLevel      string
 }
 
 type K8SClusterOnboardingCredential struct {
@@ -1783,14 +1782,12 @@ func (s *FakeConfigServer) CreateK8SCluster(ctx context.Context, req *configv1.C
 	model := &K8SCluster{
 		Id:            id,
 		IllumioRegion: req.IllumioRegion,
-		LogLevel:      req.LogLevel,
 	}
 	resp := &configv1.CreateK8SClusterResponse{
 		Id:            id,
 		ClientId:      model.ClientId,
 		ClientSecret:  model.ClientSecret,
 		IllumioRegion: model.IllumioRegion,
-		LogLevel:      model.LogLevel,
 	}
 	s.K8SClusterMutex.Lock()
 	s.K8SClusterMap[id] = model
@@ -1820,7 +1817,6 @@ func (s *FakeConfigServer) ReadK8SCluster(ctx context.Context, req *configv1.Rea
 		Id:            id,
 		ClientId:      model.ClientId,
 		IllumioRegion: model.IllumioRegion,
-		LogLevel:      model.LogLevel,
 	}
 	s.K8SClusterMutex.RUnlock()
 	s.Logger.Info("read resource",
@@ -1851,8 +1847,6 @@ func (s *FakeConfigServer) UpdateK8SCluster(ctx context.Context, req *configv1.U
 	}
 	for _, path := range updateMaskPaths {
 		switch path {
-		case "log_level":
-			model.LogLevel = req.LogLevel
 		default:
 			s.AwsAccountMutex.Unlock()
 			s.Logger.Error("attempted to update resource using invalid update_mask path",
@@ -1869,7 +1863,6 @@ func (s *FakeConfigServer) UpdateK8SCluster(ctx context.Context, req *configv1.U
 		Id:            id,
 		ClientId:      model.ClientId,
 		IllumioRegion: model.IllumioRegion,
-		LogLevel:      model.LogLevel,
 	}
 	s.K8SClusterMutex.Unlock()
 	s.Logger.Info("updated resource",
